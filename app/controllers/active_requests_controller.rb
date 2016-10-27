b5daf7613297556b3de80b821b6196a45122ab74
MarcvanMelle <marcvanmelle@gmail.com>
2016-10-27 14:10:33 -0400

    Accept Trade/Request should actually send mail now. Opps :P

--- app/controllers/active_requests_controller.rb
+++ app/controllers/active_requests_controller.rb
@@ -3,7 +3,7 @@ class ActiveRequestsController < ApplicationController
     @user_a = ActiveRequest.find(params[:id]).user
     @user_b = ActiveRequest.find(params[:id]).request.user
     if Rails.env.production?
-      AcceptMailer.accept_notification(@user_a, @user_b)
+      AcceptMailer.accept_notification(@user_a, @user_b).deliver_now
       flash[:success] = "Trade offer accepted"
     else
       flash[:errors] = "Cannot accept trades in dev-mode"
