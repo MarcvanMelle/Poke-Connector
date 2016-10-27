424e99900b91de9b0cfae443df9a78fe109456a2
MarcvanMelle <marcvanmelle@gmail.com>
2016-10-27 13:48:35 -0400

    Reworked routes for trade accepts passing the correct information

--- app/mailers/pokeball_mailer.rb
+++ app/mailers/pokeball_mailer.rb
@@ -1,10 +1,10 @@
 class PokeballMailer < ApplicationMailer
-  def pokeball_notification(user_a, user_b, offer)
+  def pokeball_notification(user_a, user_b, offer, new_trade)
     @user_a = user_a
     @user_b = user_b
     @pokeball = offer
     @url = "https://poke-connector.herokuapp.com/pokeballs/#{offer.id}"
-    @accept = "http://poke-connector.herokuapp.com/users/#{user_b.id}/accept_trade"
+    @accept = "http://poke-connector.herokuapp.com/active_pokeballs/#{new_trade.id}/accept_trade"
     mail(to: @user_a.email, subject: 'You have a new trade request!')
   end
 end
--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -1,10 +1,10 @@
 class RequestMailer < ApplicationMailer
-  def request_notification(user_a, user_b, request)
+  def request_notification(user_a, user_b, request, new_trade)
     @user_a = user_a
     @user_b = user_b
     @request = request
     @url = "https://poke-connector.herokuapp.com/requests/#{request.id}"
-    @accept = "http://poke-connector.herokuapp.com/users/#{user_b.id}/accept_trade"
+    @accept = "http://poke-connector.herokuapp.com/active_requests/#{new_trade.id}/accept_request"
     mail(to: @user_a.email, subject: 'You have a new trade request!')
   end
 end
